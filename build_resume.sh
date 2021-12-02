3cac69cbc23f533a648531938e2b2ecfbc91f50d
Alex Khaerov <i@hayorov.ru>
2021-12-02 18:22:35 +0800

    Use abs path

--- build_resume.sh
+++ build_resume.sh
@@ -2,13 +2,15 @@
 
 set -euf -o pipefail
 
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 BUILD_HASH=$(git rev-parse --short HEAD)
 BUILD_DATE=$(git show -s --format=%ci)
 
 echo "Add revision ${BUILD_HASH} | ${BUILD_DATE}"
-sed -i '' -e "s/[[GIT_COMMIT]]/${BUILD_HASH}/g" content/resume-footer.html
-sed -i '' -e "s/[[GIT_DATE]]/${BUILD_DATE}/g" content/resume-footer.html
+sed -i '' -e "s/[[GIT_COMMIT]]/${BUILD_HASH}/g" "${SCRIPTPATH}/content/resume-footer.html"
+sed -i '' -e "s/[[GIT_DATE]]/${BUILD_DATE}/g" "${SCRIPTPATH}/content/resume-footer.html"
 
 echo "Render static PDF from .md"
-mdpdf content/resume.md static/cv/alex-khaerov-resume-latest.pdf --format=A4 \
+mdpdf "${SCRIPTPATH}/content/resume.md" "${SCRIPTPATH}/static/cv/alex-khaerov-resume-latest.pdf" --format=A4 \
     --header content/resume-header.html --footer content/resume-footer.html
