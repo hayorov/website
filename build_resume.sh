72b948aad957daef5b541b2b691c6d4892f46093
Alex Khaerov <i@hayorov.ru>
2021-12-02 19:53:25 +0800

    Update tag

--- build_resume.sh
+++ build_resume.sh
@@ -11,8 +11,8 @@ BUILD_HASH=$(git rev-parse --short HEAD)
 BUILD_DATE=$(git show -s --format=%ci)
 
 echo "Add revision ${BUILD_HASH} | ${BUILD_DATE}"
-sed -i -e "s/[[GIT_COMMIT]]/${BUILD_HASH}/g" "${SCRIPTPATH}/content/resume-footer.html"
-sed -i -e "s/[[GIT_DATE]]/${BUILD_DATE}/g" "${SCRIPTPATH}/content/resume-footer.html"
+sed -i -e "s/GIT_COMMIT/${BUILD_HASH}/g" "${SCRIPTPATH}/content/resume-footer.html"
+sed -i -e "s/GIT_DATE/${BUILD_DATE}/g" "${SCRIPTPATH}/content/resume-footer.html"
 
 echo "Render static PDF from .md"
 mdpdf "${SCRIPTPATH}/content/resume.md" "${SCRIPTPATH}/static/cv/alex-khaerov-resume-latest.pdf" --format=A4 \
