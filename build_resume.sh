84100140228aac3bcf8843d57ff289bc01bcd126
Alex Khaerov <i@hayorov.ru>
2021-12-02 18:19:57 +0800

    Alt tempalte marker

--- build_resume.sh
+++ build_resume.sh
@@ -6,8 +6,8 @@ BUILD_HASH=$(git rev-parse --short HEAD)
 BUILD_DATE=$(git show -s --format=%ci)
 
 echo "Add revision ${BUILD_HASH} | ${BUILD_DATE}"
-sed -i '' -e "s/{{GIT_COMMIT}}/${BUILD_HASH}/g" content/resume-footer.html
-sed -i '' -e "s/{{GIT_DATE}}/${BUILD_DATE}/g" content/resume-footer.html
+sed -i '' -e "s/[[GIT_COMMIT]]/${BUILD_HASH}/g" content/resume-footer.html
+sed -i '' -e "s/[[GIT_DATE]]/${BUILD_DATE}/g" content/resume-footer.html
 
 echo "Render static PDF from .md"
 mdpdf content/resume.md static/cv/alex-khaerov-resume-latest.pdf --format=A4 \
