6dd39bf8aeca84f04ee4902921aff2a4d503398c
Alex Khaerov <hayorov@users.noreply.github.com>
2021-12-02 18:37:55 +0800

    Support pdf cv generation (#18)
    * Support pdf cv generation
    
    * Alt tempalte marker
    
    * Use abs path

---
+++ build_resume.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+BUILD_HASH=$(git rev-parse --short HEAD)
+BUILD_DATE=$(git show -s --format=%ci)
+
+echo "Add revision ${BUILD_HASH} | ${BUILD_DATE}"
+sed -i '' -e "s/[[GIT_COMMIT]]/${BUILD_HASH}/g" "${SCRIPTPATH}/content/resume-footer.html"
+sed -i '' -e "s/[[GIT_DATE]]/${BUILD_DATE}/g" "${SCRIPTPATH}/content/resume-footer.html"
+
+echo "Render static PDF from .md"
+mdpdf "${SCRIPTPATH}/content/resume.md" "${SCRIPTPATH}/static/cv/alex-khaerov-resume-latest.pdf" --format=A4 \
+    --header content/resume-header.html --footer content/resume-footer.html
